9e89a3a0305d079b92babff56ddcc92ea787204a
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2018-04-26 21:08:53 +0200

    sysctl: add --cat-config
    Backport of commit 3c51c626160b4798f547ca0b2d42901909edd90f to systemd-230
    Is required to fix memory allocation in systemd-sysctl after previous backports
    (https://bugzilla.rosalinux.ru/show_bug.cgi?id=10018#c4)
    
    Signed-off-by: Mikhail Novosyolov <m.novosyolov@rosalinux.ru>

--- src/sysctl/sysctl.c
+++ src/sysctl/sysctl.c
@@ -35,11 +35,13 @@
 #include "string-util.h"
 #include "strv.h"
 #include "sysctl-util.h"
+#include "terminal-util.h"
 #include "util.h"
 
 static char **arg_prefixes = NULL;
+static bool arg_cat_config = false;
 
-static const char conf_file_dirs[] = CONF_PATHS_NULSTR("sysctl.d");
+static char **config_dirs = CONF_PATHS_STRV("sysctl.d");
 
 static int apply_all(Hashmap *sysctl_options) {
         char *property, *value;
@@ -68,7 +70,7 @@ static int parse_file(Hashmap *sysctl_options, const char *path, bool ignore_eno
 
         assert(path);
 
-        r = search_and_fopen_nulstr(path, "re", NULL, conf_file_dirs, &f);
+        r = search_and_fopen(path, "re", NULL, (const char**) config_dirs, &f);
         if (r < 0) {
                 if (ignore_enoent && r == -ENOENT)
                         return 0;
@@ -162,6 +164,7 @@ static void help(void) {
                "Applies kernel sysctl settings.\n\n"
                "  -h --help             Show this help\n"
                "     --version          Show package version\n"
+               "     --cat-config       Show configuration files\n"
                "     --prefix=PATH      Only apply rules with the specified prefix\n"
                , program_invocation_short_name);
 }
@@ -170,13 +173,15 @@ static int parse_argv(int argc, char *argv[]) {
 
         enum {
                 ARG_VERSION = 0x100,
-                ARG_PREFIX
+                ARG_CAT_CONFIG,
+                ARG_PREFIX,
         };
 
         static const struct option options[] = {
-                { "help",      no_argument,       NULL, 'h'           },
-                { "version",   no_argument,       NULL, ARG_VERSION   },
-                { "prefix",    required_argument, NULL, ARG_PREFIX    },
+                { "help",       no_argument,       NULL, 'h'            },
+                { "version",    no_argument,       NULL, ARG_VERSION    },
+                { "cat-config", no_argument,       NULL, ARG_CAT_CONFIG },
+                { "prefix",     required_argument, NULL, ARG_PREFIX     },
                 {}
         };
 
@@ -195,6 +200,10 @@ static int parse_argv(int argc, char *argv[]) {
 
                 case ARG_VERSION:
                         return version();
+
+                case ARG_CAT_CONFIG:
+                        arg_cat_config = true;
+                        break;
 
                 case ARG_PREFIX: {
                         char *p;
@@ -224,6 +233,11 @@ static int parse_argv(int argc, char *argv[]) {
                 default:
                         assert_not_reached("Unhandled option");
                 }
+
+        if (arg_cat_config && argc > optind) {
+                log_error("Positional arguments are not allowed with --cat-config");
+                return -EINVAL;
+        }
 
         return 1;
 }
@@ -262,9 +276,14 @@ int main(int argc, char *argv[]) {
                 _cleanup_strv_free_ char **files = NULL;
                 char **f;
 
-                r = conf_files_list_nulstr(&files, ".conf", NULL, conf_file_dirs);
+                r = conf_files_list_strv(&files, ".conf", NULL, (const char**) config_dirs);
                 if (r < 0) {
                         log_error_errno(r, "Failed to enumerate sysctl.d files: %m");
+                        goto finish;
+                }
+
+                if (arg_cat_config) {
+                        r = cat_files(NULL, files, 0);
                         goto finish;
                 }
 
