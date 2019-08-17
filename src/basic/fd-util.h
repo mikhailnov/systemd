a0eed83877d066031b46a2dcbc90c2856f919b65
Mikhail Novosyolov <m.novosyolov@rosalinux.ru>
2019-08-17 05:45:16 +0300

    Port fd_reopen "fd-util: introduce fd_reopen() helper for reopening"
    From upstream commit f2324783ceee84fbd24fd66f34d379fa1e6dc887
    and SUSE/v228 commit 01430a02b3d6fc9bb9f488ed54b1f443c26cdf8f to systemd-230
    
    Signed-off-by: Mikhail Novosyolov <m.novosyolov@rosalinux.ru>

--- src/basic/fd-util.h
+++ src/basic/fd-util.h
@@ -74,6 +74,8 @@ bool fdname_is_valid(const char *s);
 
 int fd_get_path(int fd, char **ret);
 
+int fd_reopen(int fd, int flags);
+
 /* Like TAKE_PTR() but for file descriptors, resetting them to -1 */
 #define TAKE_FD(fd)                             \
         ({                                      \
