d56b1d84c8eaf788f7b499544720863b476909f1
Mikhail Novosyolov <m.novosyolov@rosalinux.ru>
2019-08-17 06:52:58 +0300

    CHASE_NO_AUTOFS not implemented in systemd-230
    Signed-off-by: Mikhail Novosyolov <m.novosyolov@rosalinux.ru>

--- src/basic/fs-util.c
+++ src/basic/fs-util.c
@@ -602,7 +602,7 @@ int chase_symlinks(const char *path, const char *original_root, unsigned flags, 
         if (isempty(original_root) || path_equal(original_root, "/"))
                 original_root = NULL;
 
-        if (!original_root && !ret && (flags & (CHASE_NONEXISTENT|CHASE_NO_AUTOFS|CHASE_SAFE|CHASE_OPEN|CHASE_STEP)) == CHASE_OPEN) {
+        if (!original_root && !ret && (flags & (CHASE_NONEXISTENT|CHASE_SAFE|CHASE_OPEN|CHASE_STEP)) == CHASE_OPEN) {
                 /* Shortcut the CHASE_OPEN case if the caller isn't interested in the actual path and has no root set
                  * and doesn't care about any of the other special features we provide either. */
                 r = open(path, O_PATH|O_CLOEXEC);
