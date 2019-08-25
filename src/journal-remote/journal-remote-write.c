fd3469fb28c3af85b6155dd8191f94069432d7a0
Mikhail Novosyolov <m.novosyolov@rosalinux.ru>
2019-08-26 01:29:48 +0300

    CVE-2018-16865: journal-remote: set a limit on the number of fields in a message
    Existing use of E2BIG is replaced with ENOBUFS (entry too long), and E2BIG is
    reused for the new error condition (too many fields).
    
    This matches the change done for systemd-journald, hence forming the second
    part of the fix for CVE-2018-16865
    (https://bugzilla.redhat.com/show_bug.cgi?id=1653861).
    
    Backport of upstream commit d0696f3b7501744d91bcbef83478b9cd4a8160f4 to systemd-230
    Original author: Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
    
    Signed-off-by: Mikhail Novosyolov <m.novosyolov@rosalinux.ru>

--- src/journal-remote/journal-remote-write.c
+++ src/journal-remote/journal-remote-write.c
@@ -18,9 +18,13 @@
 ***/
 
 #include "alloc-util.h"
+#include "journald-native.h"
 #include "journal-remote.h"
 
 int iovw_put(struct iovec_wrapper *iovw, void* data, size_t len) {
+        if (iovw->count >= ENTRY_FIELD_COUNT_MAX)
+                return -E2BIG;
+
         if (!GREEDY_REALLOC(iovw->iovec, iovw->size_bytes, iovw->count + 1))
                 return log_oom();
 
